7add2ea1181e2c864db588699577e4b638e72547
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2023-10-26 13:15:59 +0530

    Update: Almost complete Clipboard API

--- app/src/main/java/com/srilakshmikanthanp/clipbirdroid/clipboard/Clipboard.kt
+++ app/src/main/java/com/srilakshmikanthanp/clipbirdroid/clipboard/Clipboard.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.core.content.FileProvider
 import com.srilakshmikanthanp.clipbirdroid.constant.appName
+import java.io.File
 
 /**
  * Clipboard Change Listener Interface
@@ -30,6 +31,29 @@ class Clipboard(private val context: Context) {
   private val MIME_TYPE_PNG: String = "image/png"
   private val MIME_TYPE_COLOR: String = "application/x-color"
   private val MIME_TYPE_HTML: String = "text/html"
+
+  /**
+   * Get the Content from the URI
+   */
+  private fun getContentFromUri(uri: Uri): Pair<String, ByteArray>? {
+    // List of Allowed Types
+    val allowedTypes = arrayOf(MIME_TYPE_TEXT, MIME_TYPE_PNG, MIME_TYPE_COLOR, MIME_TYPE_HTML)
+
+    // get the content
+    val result = context.contentResolver.openInputStream(uri).use {
+      val mimeType = context.contentResolver.getType(uri) ?: return@use null
+      val content = it?.readBytes() ?: return@use null
+      return@use Pair(mimeType, content)
+    } ?: return null
+
+    // if allowed type
+    if (!allowedTypes.contains(result.first)) {
+      return null
+    }
+
+    // return result
+    return result
+  }
 
   /**
    * ClipBoard Change Implementation
@@ -76,7 +100,7 @@ class Clipboard(private val context: Context) {
 
     // create Files for contents
     for (i in contents.indices) {
-      val file = java.io.File.createTempFile(appName(), ".tmp", context.cacheDir)
+      val file = File.createTempFile(appName(), ".tmp", context.cacheDir)
       file.writeBytes(contents[i].second)
       uris.add(FileProvider.getUriForFile(context, context.packageName, file))
     }
@@ -110,43 +134,30 @@ class Clipboard(private val context: Context) {
    * @return MutableList<Pair<String, ByteArray>>
    */
   fun getClipboardContent(): MutableList<Pair<String, ByteArray>> {
-    // get the clip data
-    val clipData = this.clipboard.primaryClip ?: return mutableListOf()
-
     // create a list of Pair<String, ByteArray>
     val contents = mutableListOf<Pair<String, ByteArray>>()
+
+    // get the clip data
+    val clipData = this.clipboard.primaryClip ?: return mutableListOf()
 
     // iterate through all the items
     for (i in 0 until clipData.itemCount) {
       // get tha Item at i
       val item = clipData.getItemAt(i)
 
+      // if has html
+      if (item.htmlText != null) {
+        contents.add(Pair(MIME_TYPE_HTML, item.htmlText.toString().toByteArray()))
+      }
+
       // if has uri
       if (item.uri != null) {
-        // List of Allowed Types
-        val allowedTypes = arrayOf(MIME_TYPE_TEXT, MIME_TYPE_PNG, MIME_TYPE_COLOR, MIME_TYPE_HTML)
-
-        // get the content
-        val result = context.contentResolver.openInputStream(item.uri).use {
-          val mimeType = context.contentResolver.getType(item.uri) ?: return@use null
-          val content = it?.readBytes() ?: return@use null
-          return@use Pair(mimeType, content)
-        } ?: continue
-
-        // if allowed type
-        if (allowedTypes.contains(result.first)) {
-          contents.add(result)
-        }
+        getContentFromUri(item.uri)?.let { contents.add(it) }
       }
 
       // if has text
       if (item.text != null) {
         contents.add(Pair(MIME_TYPE_TEXT, item.text.toString().toByteArray()))
-      }
-
-      // if has html
-      if (item.htmlText != null) {
-        contents.add(Pair(MIME_TYPE_HTML, item.htmlText.toString().toByteArray()))
       }
     }
 
