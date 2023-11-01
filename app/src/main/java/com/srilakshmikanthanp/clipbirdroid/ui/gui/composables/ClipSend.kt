37acfe71f9f846f774d95cda08aa193dc438170f
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2023-11-01 22:03:55 +0530

    Update: Addition of Some Composable

--- app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/composables/ClipSend.kt
+++ app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/composables/ClipSend.kt
@@ -1,8 +1,44 @@
 package com.srilakshmikanthanp.clipbirdroid.ui.gui.composables
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+/**
+ * ClipSend Composable
+ */
 @Composable
-fun ClipSend() {
+fun ClipSend(onSend: () -> Unit = {}, modifier: Modifier = Modifier) {
+  // Row for the Text and send Button
+  Row(
+    horizontalArrangement = Arrangement.SpaceBetween,
+    modifier = modifier,
+    verticalAlignment = Alignment.CenterVertically,
+  ) {
+    Text(
+      text = "Send the Latest Clipboard to other Devices",
+    )
+    Button(onClick = onSend) {
+      Text(text = "Send")
+    }
+  }
+}
 
+/**
+ * Preview of ClipSend
+ */
+@Preview(showBackground = true)
+@Composable
+fun ClipSendPreview() {
+  ClipSend(
+    modifier = Modifier.fillMaxWidth().padding(10.dp),
+  )
 }
--- app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/composables/Device.kt
+++ app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/composables/Device.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -15,6 +16,11 @@ import androidx.compose.ui.unit.dp
 import com.srilakshmikanthanp.clipbirdroid.types.device.Device
 import java.net.InetAddress
 import java.util.Locale
+
+/**
+ * DeviceActionable Type Alias
+ */
+typealias DeviceActionable = Pair<Device, DeviceAction>
 
 /**
  * Device Action Enum To Represent Connect or Disconnect
@@ -30,11 +36,6 @@ enum class DeviceAction {
     }
   }
 }
-
-/**
- * DeviceActionable Type Alias
- */
-typealias DeviceActionable = Pair<Device, DeviceAction>
 
 /**
  * Device Composable to Represent the Device
@@ -56,8 +57,9 @@ fun Device(
   ) {
     // Align Test the Center both horizontally and vertically
     Text(
+      style = MaterialTheme.typography.labelLarge,
       text = deviceActionable.first.name,
-      modifier = Modifier.padding(5.dp)
+      modifier = Modifier.padding(start = 10.dp)
     )
 
     // Connect or Disconnect Button
@@ -76,7 +78,7 @@ fun Device(
 @Composable
 private fun DevicePreview() {
   // A Constant Device Value to Preview
-  val device = Device(InetAddress.getLocalHost(), 8080, "LAPTOP-HP")
+  val device = Device(InetAddress.getLocalHost(), 8080, "Apple MacBook Air")
 
   // Preview the Device
   Column() {
