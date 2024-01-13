fcb4be98bc0b07a853f8c0e069b564aeb520c36e
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2024-01-13 09:07:39 +0530

    Try to fix boot receive handler

--- app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/handlers/BootHandler.kt
+++ app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/handlers/BootHandler.kt
@@ -3,12 +3,13 @@ package com.srilakshmikanthanp.clipbirdroid.ui.gui.handlers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.srilakshmikanthanp.clipbirdroid.ui.gui.service.ClipbirdService
 
 class BootHandler : BroadcastReceiver() {
   override fun onReceive(context: Context, intent: Intent) {
-    if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
-      ClipbirdService.start(context)
-    }
+    Log.d("BootHandler", "Starting Service: ${intent.action}")
+    ClipbirdService.start(context)
+    Log.d("BootHandler", "Started Service: ${intent.action}")
   }
 }
