7095078c0f7766b5bf46f957db94afcdccfc8bdb
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2024-03-25 18:09:29 +0530

    Fixes #5 regarding status bar and nav bar color

--- app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/theme/Theme.kt
+++ app/src/main/java/com/srilakshmikanthanp/clipbirdroid/ui/gui/theme/Theme.kt
@@ -14,7 +14,9 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.colorResource
 import androidx.core.view.WindowCompat
+import com.srilakshmikanthanp.clipbirdroid.R
 
 private val lightScheme = lightColorScheme(
   primary = primaryLight,
@@ -107,14 +109,18 @@ fun ClipbirdTheme(
     else -> lightScheme
   }
 
+  val bgColor = colorResource(id = R.color.secondary).toArgb()
   val view = LocalView.current
 
   if (!view.isInEditMode) {
     SideEffect {
       val window = (view.context as Activity).window
       window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-      window.statusBarColor = colorScheme.background.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+      window.statusBarColor = bgColor
+      window.navigationBarColor = bgColor
+      val insets = WindowCompat.getInsetsController(window, view)
+      insets.isAppearanceLightStatusBars = !darkTheme
+      insets.isAppearanceLightNavigationBars = !darkTheme
     }
   }
 
