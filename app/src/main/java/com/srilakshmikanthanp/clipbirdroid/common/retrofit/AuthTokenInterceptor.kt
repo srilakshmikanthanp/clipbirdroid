61bfa6eb2e939f79cdfe604be0d4886b66f4db60
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-22 11:38:06 +0530

    Add AuthTokenInterceptor to manage JWT token in requests

---
+++ app/src/main/java/com/srilakshmikanthanp/clipbirdroid/common/retrofit/AuthTokenInterceptor.kt
@@ -0,0 +1,19 @@
+package com.srilakshmikanthanp.clipbirdroid.common.retrofit
+
+import com.srilakshmikanthanp.clipbirdroid.storage.Storage
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthTokenInterceptor @Inject constructor(
+  private val storage: Storage
+): Interceptor {
+  override fun intercept(chain: Interceptor.Chain): Response {
+    val jwtToken = storage.getHubAuthToken()
+    val requestBuilder = chain.request().newBuilder()
+    if (jwtToken != null) {
+      requestBuilder.addHeader("Authorization", "Bearer ${jwtToken.token}")
+    }
+    return chain.proceed(requestBuilder.build())
+  }
+}
