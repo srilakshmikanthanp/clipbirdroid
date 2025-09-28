9a82dd706c56b4dbe2f68df2caa6bc5e68691dcd
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2025-09-28 12:13:53 +0530

    Remove unused imports in Clipbird.kt

--- app/src/main/java/com/srilakshmikanthanp/clipbirdroid/Clipbird.kt
+++ app/src/main/java/com/srilakshmikanthanp/clipbirdroid/Clipbird.kt
@@ -1,26 +1,15 @@
 package com.srilakshmikanthanp.clipbirdroid
 
 import android.app.Application
-import android.content.Context
 import com.google.android.gms.common.moduleinstall.ModuleInstall
 import com.google.android.gms.common.moduleinstall.ModuleInstallRequest
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
-import com.srilakshmikanthanp.clipbirdroid.common.functions.generateX509Certificate
-import com.srilakshmikanthanp.clipbirdroid.constants.appCertExpiryInterval
-import com.srilakshmikanthanp.clipbirdroid.constants.appMdnsServiceName
 import com.srilakshmikanthanp.clipbirdroid.clipboard.ClipboardController
 import com.srilakshmikanthanp.clipbirdroid.history.HistoryController
+import com.srilakshmikanthanp.clipbirdroid.storage.Storage
 import com.srilakshmikanthanp.clipbirdroid.syncing.lan.LanController
 import com.srilakshmikanthanp.clipbirdroid.syncing.wan.WanController
-import com.srilakshmikanthanp.clipbirdroid.storage.Storage
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
-import org.bouncycastle.asn1.x500.style.BCStyle
-import org.bouncycastle.asn1.x500.style.IETFUtils
-import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder
-import java.security.PrivateKey
-import java.security.cert.X509Certificate
 import javax.inject.Inject
 
 @HiltAndroidApp
